7b3b4aed193bfb463ec40eaafaae63a71c328775
Michel Albert <michel@albert.lu>
2012-06-29 17:57:04 +0200

    Syntax fixed.

--- lu/albert/closure/fx/easing/easing.js
+++ lu/albert/closure/fx/easing/easing.js
@@ -69,9 +69,9 @@ lu.albert.closure.fx.easing.Back.easeInOut = function(t, b, c, d, s) {
     s = 1.70158;
   }
   if ((t /= d / 2) < 1) {
-    return c / 2 * (t * t * (((s * = (1.525)) + 1) * t - s)) + b;
+    return c / 2 * (t * t * (((s *= (1.525)) + 1) * t - s)) + b;
   }
-  return c / 2 * ((t -= 2) * t * (((s * = (1.525)) + 1) * t + s) + 2) + b;
+  return c / 2 * ((t -= 2) * t * (((s *= (1.525)) + 1) * t + s) + 2) + b;
 };
 
 
